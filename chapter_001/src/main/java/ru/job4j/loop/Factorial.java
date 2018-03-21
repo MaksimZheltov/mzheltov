d4a714ffc94693fe8674b75dc8bde7f041de4209
Maksim Zheltov <mvzheltov@yandex.ru>
2018-03-21 23:03:59 +0400

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+/**
+ * Counter.
+ * @author Maksim Zheltov (mvzheltov@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Factorial {
+
+    public int calc(int n) {
+        int multiply = 1;
+        if (n < 1) return 1;
+            else
+                for (int index = 1; index <= n; index++) {
+                    multiply = multiply * index;
+                }
+                return multiply;
+        }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        Factorial multiply = new Factorial();
+        assertThat(multiply.calc(5), is(120));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        Factorial multiply = new Factorial();
+        assertThat(multiply.calc(0), is(1));
+    }
+
+    @Test
+    public void whenCalculateFactorialForOneThenOne() {
+        Factorial multiply = new Factorial();
+        assertThat(multiply.calc(1), is(1));
+    }
+
+}
